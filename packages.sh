3fcc688b9dbf7352eb4cb7d003c14b1f73f95661
syswraith <syswraith@proton.me>
2025-01-30 20:11:54 +0530

    package update

--- packages.sh
+++ packages.sh
@@ -1,2 +1,2 @@
-#!/usr/bin/env bash 
+#!/usr/bin/env bash
 pacman -Qei | awk '/^Name/ { name=$3 } /^Groups/ { if ( $3 != "base" && $3 != "base-devel" ) { print name } }' > ./packages.txt && git add . && git commit -m "package update" && git push 
